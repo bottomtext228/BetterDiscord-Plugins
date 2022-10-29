2bde73686a8890f7aff217f13cec85417846311e
bottomtext228 <77897606+bottomtext228@users.noreply.github.com>
2022-10-29 16:30:42 +0300

    Add files via upload

---
+++ Plugins/FreeStickers/FreeStickers.plugin.js
@@ -0,0 +1,101 @@
+/**
+ * @name FreeStickers
+ * @version 2.0.0
+ * @author bottom_text | Z-Team 
+ * @description Makes available to send not animated stickers and emojies everywhere like with nitro.
+*/
+
+
+module.exports = class FreeStickers {
+    getName() {
+        return 'FreeStickers'
+    }
+    getVersion() {
+        return '2.0.0'
+    }
+    getAuthor() {
+        return 'bottom_text | Z-Team';
+    }
+    getDescription() {
+        return 'Makes available to send not animated stickers and emojies everywhere like with nitro.'
+    }
+    start() {
+        console.log(`${this.getName()}: started!`);
+
+        this.findWebpacks();
+
+        BdApi.Patcher.after(this.getName(), this.permissionsWebpack, 'canUseStickersEverywhere', (_, args, ret) => {
+            return true;
+        });
+
+        BdApi.Patcher.after(this.getName(), this.permissionsWebpack, 'canUseEmojisEverywhere', (_, args, ret) => {
+            return true;
+        });
+
+
+        /*
+        BdApi.Patcher.after(this.emojiWebpack, 'getEmojiUnavailableReason', (_, args, ret) => {  
+           // we broke this with canUseStickerEverywhere hook 
+            return ret;
+        })
+        */
+       
+        BdApi.Patcher.instead(this.getName(), this.enqueWebpack, 'enqueue', (_, args, original) => {
+            const message = args[0].message;
+
+            /* stickers */
+            if (message.sticker_ids && !this.isStickerAvailable(message.sticker_ids[0], message.channelId)) {
+                message.content = `https://media.discordapp.net/stickers/${message.sticker_ids[0]}.webp?size=160`; // insert sticker url in the message
+                delete message.sticker_ids;
+            }
+
+            /* emojies */
+            const regExp = /<:\w+:(\d+)>/g; // guild emoji 
+            let match;
+            while (match = regExp.exec(message.content)) {
+                const emojiId = match[1];
+                const customEmoji = this.getCustomEmojiById(emojiId);             
+                if (customEmoji && !this.isCustomEmojiAvailable(customEmoji, message.channelId)) {
+                    message.content = message.content.replace(match[0], this.getCustomEmojiById(emojiId).url); // replace emoji code with url
+                }
+            }
+
+            original(...args);
+        });
+
+    }
+    isStickerAvailable(stickerId, channelId) {
+        //  not the best method, still works
+        return this.stickerWebpack.getStickerById(stickerId).guild_id == this.getChannel(channelId).guild_id
+    }
+    isCustomEmojiAvailable(customEmoji, channelId) {
+        // just check if we send guild emoji in the guild with this emoji; not the best method, still works
+        return customEmoji.guildId == this.getChannel(channelId).guild_id;
+    }
+    /*  getEmojiUnavailableReason(emojiId, channelId, intention){
+         return this.emojiWebpack.getEmojiUnavailableReason({
+             emoji: this.getCustomEmojiById(emojiId),
+             channel: this.getChannel(channelId),
+             intention: 3 // magic number
+         });
+         // return value == 2 - emoji blocker | null - avalaible. (!) don't work with canUseStickersEverywhere() hook (!)
+     } */
+    getChannel(channelId) {
+        return this.channelStoreWebpack.getChannel(channelId);
+    }
+    getCustomEmojiById(emojiId) {
+        return this.customEmojieUtilities.getCustomEmojiById(emojiId);
+    }
+    findWebpacks() {
+        this.permissionsWebpack = ZeresPluginLibrary.WebpackModules.getByProps('canUseStickersEverywhere');
+        this.enqueWebpack = ZeresPluginLibrary.WebpackModules.getByProps('enqueue');
+        //this.emojiWebpack = ZeresPluginLibrary.WebpackModules.getByProps('getEmojiUnavailableReason');
+        this.channelStoreWebpack = ZeresPluginLibrary.WebpackModules.getByProps('getChannel');
+        this.customEmojieUtilities = ZeresPluginLibrary.WebpackModules.getByProps('getCustomEmojiById');
+        this.stickerWebpack = ZeresPluginLibrary.WebpackModules.getByProps('getStickerById');
+    }
+    stop() {
+        console.log(`${this.getName()}: stopped!`);
+        BdApi.Patcher.unpatchAll(this.getName());
+    }
+}
