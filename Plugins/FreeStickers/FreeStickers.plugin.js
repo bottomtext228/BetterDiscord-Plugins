3c7f8c2b794892729b27e3e8c68be8fbf10a68ed
bottomtext228 <77897606+bottomtext228@users.noreply.github.com>
2026-01-25 15:38:42 +0300

    v2.1.4 update
    Fix work due to Discord update.

--- Plugins/FreeStickers/FreeStickers.plugin.js
+++ Plugins/FreeStickers/FreeStickers.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name FreeStickers
- * @version 2.1.3
+ * @version 2.1.4
  * @author bottom_text | Z-Team 
  * @description Makes available to send stickers (not animated) and any emojis everywhere like with nitro.
  * @source https://github.com/bottomtext228/BetterDiscord-Plugins/tree/main/Plugins/FreeStickers
@@ -22,15 +22,15 @@ module.exports = class FreeStickers {
         this.findWebpacks();
 
         // removes lock from guild icons in emoji picker menu
-        const [EmojiPickerGuildLocks, key] = BdApi.Webpack.getWithKey(BdApi.Webpack.Filters.byStrings('.activeCategoryIndex)', '.scrollToSectionTop('));
+        const [EmojiPickerGuildLocks, key] = BdApi.Webpack.getWithKey(BdApi.Webpack.Filters.byStrings('.activeCategoryIndex)', 'getScrollOffsetForIndex',
+            'expressionsListRef', '.getState();if('));
         BdApi.Patcher.before(this.name, EmojiPickerGuildLocks, key, (_, [props]) => {
             props.categories.forEach(category => {
                 if (category.type == 'GUILD') {
                     category.isNitroLocked = false;
                 }
             })
-        }
-        );
+        });
 
         // removes nitro upsell in emoji picker menu
         const [EmojiPickerNitroUpsell, key1] = BdApi.Webpack.getWithKey(BdApi.Webpack.Filters.byStrings('sectionDescriptors', 'PREMIUM_UPSELL_VIEWED'));
@@ -40,8 +40,7 @@ module.exports = class FreeStickers {
                     category.isNitroLocked = false;
                 }
             });
-        }
-        );
+        });
 
         // hook to allow adding emoji
         BdApi.Patcher.after(this.name, this.emojiWebpack, 'getEmojiUnavailableReason', (_, [{ emoji, channel, intention, canViewAndUsePackEmoji, hook }], ret) => {
@@ -61,11 +60,11 @@ module.exports = class FreeStickers {
         });
 
         // hook to allow send stickers
-        BdApi.Patcher.instead(this.name, this.stickerSendabilityWebpack, 'kl', (_, [sticker, user, channel, hook], original) => {
+        BdApi.Patcher.instead(this.name, this.stickerSendabilityWebpackWithKey[0], this.stickerSendabilityWebpackWithKey[1], (_, [sticker, user, channel, hook], original) => {
             // `hook` means that we call it by ourselves
             return hook ? original(sticker, user, channel) : true;
         });
-      
+
         // change message object and replace emoji/sticker objects with its URL
         BdApi.Patcher.instead(this.name, this.enqueWebpack, 'enqueue', (_, args, original) => {
             const message = args[0].message;
@@ -90,7 +89,7 @@ module.exports = class FreeStickers {
             }
 
             original(...args);
-        }); 
+        });
 
     }
 
@@ -98,8 +97,7 @@ module.exports = class FreeStickers {
         const currentUser = this.userStoreWebpack.getCurrentUser();
         const channel = this.channelStoreWebpack.getChannel(channelId);
         const sticker = this.stickerWebpack.getStickerById(stickerId);
-        return this.stickerSendabilityWebpack.kl(sticker, currentUser, channel, true);
-
+        return this.stickerSendabilityWebpackWithKey[0][this.stickerSendabilityWebpackWithKey[1]](sticker, currentUser, channel, true);
     }
 
     isCustomEmojiAvailable(customEmoji, channelId) {
@@ -122,8 +120,23 @@ module.exports = class FreeStickers {
         this.channelStoreWebpack = BdApi.Webpack.getStore('ChannelStore');
         this.customEmojiUtilitiesWebpack = BdApi.Webpack.getByKeys('getCustomEmojiById');
         this.stickerWebpack = BdApi.Webpack.getByKeys('getStickerById');
-        // const [StickerSendability, getStickerSendability] = BdApi.Webpack.getWithKey(BdApi.Webpack.Filters.byStrings('canUseCustomStickersEverywhere', 'USE_EXTERNAL_STICKERS'))
-        this.stickerSendabilityWebpack = BdApi.Webpack.getByKeys('kl', 'cO'); // kl = isSendableSticker, cO = getStickerSendability
+
+        /**
+         * StickerSendability module consists of three entries. 
+         * 
+         * STICKER_SENDABILITY enum
+         * isSendableSticker <- what we need to patch
+         * getStickerSendability 
+         * 
+         * isSendableSticker is hard to find because it is just `(e,t,n)=>0===u(e,t,n)` 
+         * unmangled: (sticker, user, channel) => STICKER_SENDABILITY.SENDABLE === getStickerSendability(sticker, user, channel)  
+         * but it is much easier to find getStickerSendability using strings and then we iterate over StickerSendability module and exclude others two.
+         */
+        const [StickerSendability, getStickerSendability] = BdApi.Webpack.getWithKey(BdApi.Webpack.Filters.byStrings('canUseCustomStickersEverywhere', 'USE_EXTERNAL_STICKERS'));
+        const isStickerAvailable = Object.entries(StickerSendability).find((key, value) => !value.NONSENDABLE && value !== getStickerSendability)[0];
+
+        // [module, key]
+        this.stickerSendabilityWebpackWithKey = [StickerSendability, isStickerAvailable];
     }
 
     stop() {
